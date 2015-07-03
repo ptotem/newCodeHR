84a47f84583170a14add9e3dc14c5472913b78a3
ankitgandhi452 <ankitgandhi452@gmail.com>
2015-07-03 17:08:16 +0530

    notify

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,11 +2,14 @@ class NotificationsController < InheritedResources::Base
 
 	def show
 		@notification = Notification.find(params[:id])
+		@notification.read = true
+		@notification.save!
 	end
 
 	def destroy
-		render :json => params
-		return
+		notification = Notification.find(params[:id])
+		notification.destroy
+		redirect_to notification
 	end
 
   private
--- app/models/task.rb
+++ app/models/task.rb
@@ -7,7 +7,6 @@ class Task
   field :description, type: String
   field :link, type: String
   field :read, type: Boolean, :default => false
-  field :erased, type: Boolean, :default => false
 
   belongs_to :user
 end
