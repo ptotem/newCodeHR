213ce95af96e33cc908eb07879b32ec96c918319
agarwalmehul <agarwalmehul92@gmail.com>
2015-07-06 13:07:03 +0530

    executing process - WIP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,49 @@ require 'set'
 module ApplicationHelper
 	def step_instance_processing(process_instance)
 		puts process_instance.to_json
+		# process_instance.end_processing_step
+		eval("process_step_"+process_instance[:action])(process_instance)
+	end
+
+	def process_step_Fill(process_instance)
+		puts "############################################################"
+		puts "Fill Step"
+		puts "############################################################"
+		process_instance.end_processing_step
+	end
+
+	def process_step_Approve(process_instance)
+		puts "############################################################"
+		puts "Approve Step"
+		puts "############################################################"
+		process_instance.end_processing_step
+	end
+
+	def process_step_Notify(process_instance)
+		puts "############################################################"
+		puts "Notify Step"
+		puts "############################################################"
+		process_instance.end_processing_step
+	end
+
+	def process_step_MarkComplete(process_instance)
+		puts "############################################################"
+		puts "MarkComplete Step"
+		puts "############################################################"
+		process_instance.end_processing_step
+	end
+
+	def process_step_SpawnD(process_instance)
+		puts "############################################################"
+		puts "SpawnD Step"
+		puts "############################################################"
+		process_instance.end_processing_step
+	end
+
+	def process_step_SpawnI(process_instance)
+		puts "############################################################"
+		puts "SpawnI Step"
+		puts "############################################################"
 		process_instance.end_processing_step
 	end
 end
--- app/models/master_step.rb
+++ app/models/master_step.rb
@@ -5,12 +5,12 @@ class MasterStep
   field :sequence, type: Integer
   field :action, type: String
   field :action_class, type: String
-  # field :action_obj, type: String
+  field :action_obj, type: Hash
   # field :auto, type: Boolean
   # field :action_parameter, type: Array
   # field :params_mapping, type: Hash
-  field :approval_obj, type: Hash
-  field :notification_obj, type: Hash
+  # field :approval_obj, type: Hash
+  # field :notification_obj, type: Hash
   field :erased, type: Boolean
   field :repeat_on, type: String
   # field :reporting_officer, type: Boolean
