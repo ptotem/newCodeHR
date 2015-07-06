3726cf711b79092e53459440dfcabceef2076c26
agarwalmehul <agarwalmehul92@gmail.com>
2015-07-06 19:37:08 +0530

    file upload WIP

--- app/controllers/generics_controller.rb
+++ app/controllers/generics_controller.rb
@@ -1,9 +1,24 @@
 class GenericsController < InheritedResources::Base
 
+	def new
+		@generic = Generic.new
+		@d = []
+
+		gon.d = @d
+		@form_title = "Fill Form - Document Creation"
+		
+	end
+
+	def create
+		render :text => params
+		return
+		
+	end
+
   private
 
-    def generic_params
-      params.require(:generic).permit()
-    end
+    # def generic_params
+    #   params.require(:generic).permit()
+    # end
 end
 
