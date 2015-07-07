dd0bcf88aebe2836bb9855231767b8e877cfcb8a
ankitgandhi452 <ankitgandhi452@gmail.com>
2015-07-07 18:08:54 +0530

    notice test wip

--- app/controllers/generics_controller.rb
+++ app/controllers/generics_controller.rb
@@ -1,10 +1,14 @@
 class GenericsController < InheritedResources::Base
 
 	def new
+		gon.notice = session[:notice]
 		@generic = Generic.new
 		@model = "Document"
 		@step_instance = {_id: "111"}
 		@form = Generic.find_by(model: @model)
+		session[:notice] = [{type: 'success', description: 'test'},{type: 'success',title: 'test1', description: 'test'}]
+
+		redirect_to generic_path(@generic._id.to_s)
 		gon.form = @form
 		
 	end
@@ -19,6 +23,11 @@ class GenericsController < InheritedResources::Base
 		render :json => params
 		return
 		
+	end
+	def show
+		gon.notice = session[:notice]
+		# session[:notice] = nil
+		# redirect_to root_path
 	end
 
   private
