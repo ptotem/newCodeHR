0aa4ac57f15597a7d72f716d0b87503575fba2c7
ankitgandhi452 <ankitgandhi452@gmail.com>
2015-07-09 13:24:11 +0530

    manpower creation done

--- app/models/manpower_planning.rb
+++ app/models/manpower_planning.rb
@@ -6,9 +6,10 @@ class ManpowerPlanning
   field :name, type: String
   field :description, type: String
 
-  field :location, type: String
-  field :band, type: String
   field :role, type: String
+  field :department, type: String
+  field :band, type: String
+  field :location, type: String
   field :source, type: String
   field :nature, type: String
   field :vacancy, type: Integer
