b559b92f68011e7263ab1825cecdababcd218798
ankitgandhi452 <ankitgandhi452@gmail.com>
2015-07-10 13:08:35 +0530

    spwanD wip

--- app/controllers/process_instances_controller.rb
+++ app/controllers/process_instances_controller.rb
@@ -8,46 +8,46 @@ class ProcessInstancesController < InheritedResources::Base
 	def create
 		# render :json => params
 		# return
-		@process_master = ProcessMaster.find(params[:process_master])
-		@master_steps = @process_master.master_steps
+		# @process_master = ProcessMaster.find(params[:process_master])
+		# @master_steps = @process_master.master_steps
 
-		processInstance = duplicateModelObject(@process_master)
-		processInstance['process_master_id'] = @process_master._id.to_s
-		processInstance['initiator'] = current_user._id
+		# processInstance = duplicateModelObject(@process_master)
+		# processInstance['process_master_id'] = @process_master._id.to_s
+		# processInstance['initiator'] = current_user._id
 
-		stepInstances = []
-		@master_steps.each do |step|
-			_step = duplicateModelObject(step)
-			if _step['action_obj']
-				_step['action_obj']['agents']['users'] = getUsersFromAgents(_step['action_obj']['agents']['model'], _step['action_obj']['agents']['ids'])
+		# stepInstances = []
+		# @master_steps.each do |step|
+		# 	_step = duplicateModelObject(step)
+		# 	if _step['action_obj']
+		# 		_step['action_obj']['agents']['users'] = getUsersFromAgents(_step['action_obj']['agents']['model'], _step['action_obj']['agents']['ids'])
 				
-				if _step['action_obj']['manager'] 
-					current_emp = current_user.employee
-					if current_emp
-						current_emp['managers'].each do |manager|
-							_step['action_obj']['agents']['users'] << manager.user._id unless _step['action_obj']['agents']['users'].include?(manager.user._id)
-						end
-					end
-				end
+		# 		if _step['action_obj']['manager'] 
+		# 			current_emp = current_user.employee
+		# 			if current_emp
+		# 				current_emp['managers'].each do |manager|
+		# 					_step['action_obj']['agents']['users'] << manager.user._id unless _step['action_obj']['agents']['users'].include?(manager.user._id)
+		# 				end
+		# 			end
+		# 		end
 
-				if _step['action_obj']['initiator'] 
-					_step['action_obj']['agents']['users'] << current_user._id unless _step['action_obj']['agents']['users'].include?(current_user._id)
-				end
-			end
+		# 		if _step['action_obj']['initiator'] 
+		# 			_step['action_obj']['agents']['users'] << current_user._id unless _step['action_obj']['agents']['users'].include?(current_user._id)
+		# 		end
+		# 	end
 			
-			# stepInstances.push(_step)
-			stepInstances.push(StepInstance.create!(_step))
-		end
+		# 	# stepInstances.push(_step)
+		# 	stepInstances.push(StepInstance.create!(_step))
+		# end
 
-		process_instance = ProcessInstance.create!(processInstance)
-		process_instance.step_instances = stepInstances
+		# process_instance = ProcessInstance.create!(processInstance)
+		# process_instance.step_instances = stepInstances
 
-		# render :json => stepInstances
-		# render :json => process_instance.step_instances
-		# return
+		# # render :json => stepInstances
+		# # render :json => process_instance.step_instances
+		# # return
 		
-		process_instance.save!
-		process_instance.load_process
+		# process_instance.save!
+		# process_instance.load_process
 
 
 
@@ -57,6 +57,12 @@ class ProcessInstancesController < InheritedResources::Base
 		# process_instance[:initiator] = current_user._id
 		# process_instance.save!
 		# process_instance.build_process_instance
+		
+		processInstance = ProcessInstance.new
+		processInstance['process_master_id'] = params[:process_master]
+    processInstance['initiator'] = current_user._id
+    processInstance.build_process_instance 
+		
 		
 
 		redirect_to root_path
@@ -69,35 +75,35 @@ class ProcessInstancesController < InheritedResources::Base
 
   private
 
-	  def duplicateModelObject(modelObj)
-  		_modelObj = {}
-	  	modelObj.attributes.each do |key, value|
-	  		if key != '_id' && key != 'created_at' && key != 'updated_at' && key != 'process_master_id'
-	  			_modelObj[key] = value
-  			end
-  		end
-  		return _modelObj
-	  end
+	  # def duplicateModelObject(modelObj)
+  	# 	_modelObj = {}
+	  # 	modelObj.attributes.each do |key, value|
+	  # 		if key != '_id' && key != 'created_at' && key != 'updated_at' && key != 'process_master_id'
+	  # 			_modelObj[key] = value
+  	# 		end
+  	# 	end
+  	# 	return _modelObj
+	  # end
 
-	  def getUsersFromAgents(model, array)
-  		users = []
-  		if model != "Employee"
-  			if !model.empty?
-  				modelValues = eval(model).find(array)
-	  			modelValues.each do |modelValue|
-	  				modelValue.employees.each do |employee|
-	  					users.push(employee.user._id)
-						end
-					end
-  			end
-			else
-				array.each do |employee|
-					users.push(Employee.find(employee).user._id)
-				end
-  		end
+	  # def getUsersFromAgents(model, array)
+  	# 	users = []
+  	# 	if model != "Employee"
+  	# 		if !model.empty?
+  	# 			modelValues = eval(model).find(array)
+	  # 			modelValues.each do |modelValue|
+	  # 				modelValue.employees.each do |employee|
+	  # 					users.push(employee.user._id)
+			# 			end
+			# 		end
+  	# 		end
+			# else
+			# 	array.each do |employee|
+			# 		users.push(Employee.find(employee).user._id)
+			# 	end
+  	# 	end
 
-  		return users
-  	end
+  	# 	return users
+  	# end
 
   #   def process_instance_params
   #     params.require(:process_instance).permit()
--- app/models/process_instance.rb
+++ app/models/process_instance.rb
@@ -131,53 +131,67 @@ class ProcessInstance
     self.step_instances.where(sequence: sequence).first.terminate_step
   end
 
-  # def build_process_instance
-  #   process_master = ProcessMaster.find(self.process_master_id)
-  #   master_steps = process_master.master_steps
-
-  #   self = duplicateModelObject(process_master)
-
-  #   stepInstances = []
-  #   master_steps.each do |step|
-  #     _step = duplicateModelObject(step)
-  #     if _step['action_obj']
-  #       _step['action_obj']['agents']['users'] = getUsersFromAgents(_step['action_obj']['agents']['model'], _step['action_obj']['agents']['ids'])
-        
-  #       if _step['action_obj']['manager'] 
-  #         current_emp = current_user.employee
-  #         if current_emp
-  #           current_emp['managers'].each do |manager|
-  #             _step['action_obj']['agents']['users'] << manager.user._id unless _step['action_obj']['agents']['users'].include?(manager.user._id)
-  #           end
-  #         end
-  #       end
-
-  #       if _step['action_obj']['initiator'] 
-  #         _step['action_obj']['agents']['users'] << current_user._id unless _step['action_obj']['agents']['users'].include?(current_user._id)
-  #       end
-  #     end
-      
-  #     # stepInstances.push(_step)
-  #     stepInstances.push(StepInstance.create!(_step))
-  #   end
-
-  #   process_instance = ProcessInstance.create!(processInstance)
-  #   process_instance.step_instances = stepInstances
-
-  #   # render :json => stepInstances
-  #   # render :json => process_instance.step_instances
-  #   # return
+  def build_process_instance
+    process_master = ProcessMaster.find(self.process_master_id)
+    master_steps = process_master.master_steps
+    processInstanceDuplicate = duplicateModelObject(process_master)
     
-  #   process_instance.save!
-  #   process_instance.load_process
-  # end
+    self.update_attributes(processInstanceDuplicate)
 
-  # def duplicateModelObject(modelObj)
-  #     modelObj.attributes.each do |key, value|
-  #       if key != '_id' && key != 'created_at' && key != 'updated_at' && key != 'process_master_id'
-  #         self[key] = value
-  #       end
-  #     end
-  #   end
+    stepInstances = []
+    master_steps.asc(:sequence).each do |step|
+      _step = duplicateModelObject(step)
+      if _step['action_obj']
+        _step['action_obj']['agents']['users'] = getUsersFromAgents(_step['action_obj']['agents']['model'], _step['action_obj']['agents']['ids'])
+        
+        if _step['action_obj']['manager'] 
+          current_emp = current_user.employee
+          if current_emp
+            current_emp['managers'].each do |manager|
+              _step['action_obj']['agents']['users'] << manager.user._id unless _step['action_obj']['agents']['users'].include?(manager.user._id)
+            end
+          end
+        end
+
+        if _step['action_obj']['initiator'] 
+          _step['action_obj']['agents']['users'] << current_user._id unless _step['action_obj']['agents']['users'].include?(current_user._id)
+        end
+      end
+      self.step_instances.push(StepInstance.create!(_step))
+    end
+
+    self.save!
+    self.load_process
+  end
+
+  def duplicateModelObject(modelObj)
+    _modelObj = {}
+    modelObj.attributes.each do |key, value|
+      if key != '_id' && key != 'created_at' && key != 'updated_at' && key != 'process_master_id'
+        _modelObj[key] = value
+      end
+    end
+    return _modelObj
+  end
+
+  def getUsersFromAgents(model, array)
+      users = []
+      if model != "Employee"
+        if !model.empty?
+          modelValues = eval(model).find(array)
+          modelValues.each do |modelValue|
+            modelValue.employees.each do |employee|
+              users.push(employee.user._id)
+            end
+          end
+        end
+      else
+        array.each do |employee|
+          users.push(Employee.find(employee).user._id)
+        end
+      end
+
+      return users
+    end
 
 end
