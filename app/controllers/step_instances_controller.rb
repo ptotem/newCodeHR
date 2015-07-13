c05018358e185bbac3ba6533678c237687e4a86f
ankitgandhi452 <ankitgandhi452@gmail.com>
2015-07-13 12:04:30 +0530

    show page approval

--- app/controllers/step_instances_controller.rb
+++ app/controllers/step_instances_controller.rb
@@ -5,6 +5,19 @@ class StepInstancesController < InheritedResources::Base
   	@approver = step.approval.approvers.where(:user_id => current_user._id.to_s).first
   	fill_step = step.get_previous_step({action: "Fill"})
   	@path = new_generic_form_path(fill_step._id)
+
+
+    @generic = Generic.new
+    @step_instance = StepInstance.find(fill_step._id)
+    @model = @step_instance[:action_class]
+
+    @form_config = YAML::load_file("#{Rails.root}/config/forms/#{@model}.yml")
+    @form_title = @form_config['title']
+    gon.notice = session[:notice]
+    gon.form_config = @form_config
+    gon.model = {}
+    gon.model['Role'] = Role.all
+    gon.model['Band'] = Band.all
   	# render :json => path
   	# return
   end
